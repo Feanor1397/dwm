ae1ff57b697e7180d8d81c0c696b1d2c23015f2e
Feanor1397 <feanor1397@gmail.com>
2017-03-03 02:54:41 +0400

    Colors, fonts, icons

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 pkgname=dwm
 _pkgname=dwm
 _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pkgver=6.1.25.g7e4fd2b
+pkgver=6.1.26.g5bd6ae9
 pkgrel=1
 pkgdesc="A dynamic window manager for X"
 url="http://dwm.suckless.org"
--- config.h
+++ config.h
@@ -12,25 +12,31 @@ static const int voldelta = 5;
 #endif
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "dina:size=11", "noto sans:size=11", "Awesome:size=11" };
+static const char dmenufont[]       = "dina:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+
+static const char col_bg[]          = "#1b1d1f";
+static const char col_br[]          = "#000000";
+static const char col_white[]       = "#ffffff";
 static const char *colors[][3]      = {
   /*               fg         bg         border   */
+//  [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+//  [SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
   [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-  [SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
+  [SchemeSel] =  { col_white, col_br, col_cyan  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
@@ -60,12 +66,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenudesktop[]  = { "j4-dmenu-desktop", NULL };
 static const char *termcmd[]  = { "termite", NULL };
+static const char *scrot[]  = { "scrot '%Y-%m-%d_%T_scrot.png' -e 'mv $f ~/Картинки/Screenshots/'", NULL };
 
 static Key keys[] = {
   /* modifier                     key        function        argument */
   { MODKEY|ShiftMask,             XK_d,      spawn,          {.v = dmenucmd } },
   { MODKEY,                       XK_d,      spawn,          {.v = dmenudesktop } },
   { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+  { 0,                            XK_Print,  spawn,          {.v = scrot } },
   { MODKEY,                       XK_b,      togglebar,      {0} },
   { MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
   { MODKEY|ShiftMask,             XK_Tab,    focusstack,     {.i = -1 } },
